6fb73aff85444f3b2627c34c90041e6139ad1775
BrianLozach <brian.lozach@free.fr>
2018-03-13 16:54:02 +0100

    Cleaning of CrossValidation

--- Machine_Learning/Cross_Validation.py
+++ Machine_Learning/Cross_Validation.py
@@ -6,7 +6,7 @@ from sklearn import preprocessing
 from sklearn import neighbors, metrics
 
 data = pd.read_csv('C:/Users/Brian/Documents/MachineLearning/Cross-Validation_Vines/winequality-white.csv', sep=";")
-print(data.head())
+# print(data.head())
 
 X = data.as_matrix(data.columns[:-1])
 y = data.as_matrix([data.columns[-1]])
@@ -37,29 +37,21 @@ for feat_idx in range(X_train_std.shape[1]):
     ax.set_title(data.columns[feat_idx], fontsize=14)
 
 # Fixer les valeurs des hyperparamètres à tester
-
 param_grid = {'n_neighbors': [3, 5, 7, 9, 11, 13, 15]}
 
 # Choisir un score à optimiser, ici l'accuracy (proportion de prédictions correctes)
-
 score = 'accuracy'
 
 # Créer un classifieur kNN avec recherche d'hyperparamètre par validation croisée
-
 clf = model_selection.GridSearchCV(neighbors.KNeighborsClassifier(),  # un classifieur kNN
-
                                    param_grid,  # hyperparamètres à tester
-
                                    cv=5,  # nombre de folds de validation croisée
-
                                    scoring=score  # score à optimiser
-
                                    )
 
 # Optimiser ce classifieur sur le jeu d'entraînement
 
 # clf.fit(X_train, y_train)
-
 clf.fit(X_train_std, y_train)
 
 # Afficher le(s) hyperparamètre(s) optimaux
