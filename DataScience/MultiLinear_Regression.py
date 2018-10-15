24dc8f0c3bf5375bf3eaf07b35e7cf24b5b6d45e
brianlz <brian.lozach@free.fr>
2018-10-15 18:02:18 +0200

    Few changes on Multilinear Regression to fit with the venv

--- DataScience/MultiLinear_Regression.py
+++ DataScience/MultiLinear_Regression.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 from sklearn import linear_model
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 # for polynomial regression
@@ -49,7 +49,7 @@ def increasing_function(L):
 
 
 # %% Chargement du dataset
-house_data_raw = pd.read_csv('house_data.csv')
+house_data_raw = pd.read_csv('/Users/brianlz/Documents/MachineLearning/Regression_Loyer/house.csv')
 house_data = house_data_raw[house_data_raw['price'] < 7000]
 arr01_data = house_data[house_data['arrondissement'] == 1]
 arr02_data = house_data[house_data['arrondissement'] == 2]
@@ -57,7 +57,7 @@ arr03_data = house_data[house_data['arrondissement'] == 3]
 arr04_data = house_data[house_data['arrondissement'] == 4]
 arr10_data = house_data[house_data['arrondissement'] == 10]
 
-# Restructuration du dataset en par arrondissement
+# Restructuration du dataset par arrondissement
 listarr = list()
 listarr.append(arr01_data)
 listarr.append(arr02_data)
@@ -65,21 +65,21 @@ listarr.append(arr03_data)
 listarr.append(arr04_data)
 listarr.append(arr10_data)
 
-# %% Regression Linéaire générale
+# %% Regression Linéaire générale *******************************************************************************
 
 # X = np.matrix([np.ones(house_data.shape[0]), house_data['surface']]).T
 # y = np.matrix(house_data['price']).T
 #
-##Apprentissage du modèle
+# #Apprentissage du modèle
 # strain, stest, ptrain, ptest = train_test_split(X,y, train_size=0.8)
 # regr = linear_model.LinearRegression()
 # regr.fit(strain,ptrain)
 # regr.predict(stest)
 #
-##Performance
+# #Performance
 # perf(regr,stest,ptest)
 #
-##Affichage
+# #Affichage
 # plt.plot(house_data['surface'], house_data['price'], 'bo', markersize=4)
 # plt.plot(X[:,1], regr.predict(X), color='r', linewidth=2)
 # plt.title('Régression linéaire simple')
@@ -87,7 +87,7 @@ listarr.append(arr10_data)
 # plt.ylabel('Prix')
 # plt.show()
 
-# %% Regressions sur chaque arrondissement
+# %% Regressions sur chaque arrondissement *******************************************************************************
 
 # fig = plt.figure()
 # fig.set_size_inches(12.5, 7.5)
@@ -121,72 +121,72 @@ listarr.append(arr10_data)
 # ax.set_title('Regression Linéaire Spécifique')
 
 
-# %% Polynomial regression
+# %% Polynomial regression *******************************************************************************
 
-fig = plt.figure()
-fig.set_size_inches(12.5, 7.5)
-cx = fig.add_subplot(111, projection='3d')
-cx.scatter(xs=house_data['surface'], ys=house_data['arrondissement'], zs=house_data['price'])
-
-# Generation des points utilisés dans la représentation
-x_plot = np.linspace(0, 200, 100)
-X_plot = x_plot[:, np.newaxis]
-
-for arr in listarr:
-
-    # Initialisation
-    degrees = [1, 2, 3, 4, 5]
-    MAPE = 0.5
-    best_degree = 1
-
-    X = np.matrix(arr['surface']).T
-    y = np.matrix(arr['price']).T
-
-    plt.figure('Arrondissement ' + str(int(arr.values[0][2])), figsize=(14, 5))
-
-    for i in degrees:
-
-        bx = plt.subplot(1, len(degrees), i)
-        plt.setp(bx, xticks=(), yticks=())
-
-        # Apprentissage
-        model = make_pipeline(PolynomialFeatures(i), LinearRegression())
-        strain, stest, ptrain, ptest = train_test_split(X, y, train_size=0.8)
-        model.fit(strain, ptrain)
-        y_plot = model.predict(X_plot)
-
-        # Performance
-        print("\nDegré " + str(i))
-        new_MAPE = perf(model, stest, ptest)
-
-        plt.scatter(X, y, color='navy', s=30, marker='o', label="training points")
-        plt.plot(x_plot, y_plot, label="Model", color="r")
-        plt.axis([0, 250, 0, 7000])
-        plt.xlabel("surface")
-        plt.ylabel("price")
-        plt.title("Degree {}\nMAPE = {:.3f}".format(degrees[i - 1], new_MAPE))
-        plt.legend(loc="best")
-
-        # Recherche du degré aillant la meilleur peformance (ici MAPE), avec test de croissance et limite de croissance
-        if (new_MAPE < MAPE and increasing_function(y_plot) and max(y_plot) < 13000):
-            best_degree = degrees[i - 1]
-            MAPE = new_MAPE
-            best_prediction = y_plot[:, 0]
-
-    print("Meilleur degré " + str(best_degree))
-
-    # Affichage de la meilleure courbe de regression de l'arrondissement i
-    xs = x_plot
-    ys = arr.values[0][2] * np.ones(len(x_plot))
-    zs = np.array(best_prediction)
-    cx.plot(xs, ys, zs, label='Arrondissement ' + str(int(arr.values[0][2])))
-
-cx.set_ylabel('Arrondissement');
-cx.set_xlabel('Surface');
-cx.set_zlabel('Prix')
-cx.view_init(10, -135)
-cx.legend()
-cx.set_title('Regression Polynomiale Spécifique')
+# fig = plt.figure()
+# fig.set_size_inches(12.5, 7.5)
+# cx = fig.add_subplot(111, projection='3d')
+# cx.scatter(xs=house_data['surface'], ys=house_data['arrondissement'], zs=house_data['price'])
+#
+# # Generation des points utilisés dans la représentation
+# x_plot = np.linspace(0, 200, 100)
+# X_plot = x_plot[:, np.newaxis]
+#
+# for arr in listarr:
+#
+#     # Initialisation
+#     degrees = [1, 2, 3, 4, 5]
+#     MAPE = 0.5
+#     best_degree = 1
+#
+#     X = np.matrix(arr['surface']).T
+#     y = np.matrix(arr['price']).T
+#
+#     plt.figure('Arrondissement ' + str(int(arr.values[0][2])), figsize=(14, 5))
+#
+#     for i in degrees:
+#
+#         bx = plt.subplot(1, len(degrees), i)
+#         plt.setp(bx, xticks=(), yticks=())
+#
+#         # Apprentissage
+#         model = make_pipeline(PolynomialFeatures(i), LinearRegression())
+#         strain, stest, ptrain, ptest = train_test_split(X, y, train_size=0.8)
+#         model.fit(strain, ptrain)
+#         y_plot = model.predict(X_plot)
+#
+#         # Performance
+#         print("\nDegré " + str(i))
+#         new_MAPE = perf(model, stest, ptest)
+#
+#         plt.scatter(X, y, color='navy', s=30, marker='o', label="training points")
+#         plt.plot(x_plot, y_plot, label="Model", color="r")
+#         plt.axis([0, 250, 0, 7000])
+#         plt.xlabel("surface")
+#         plt.ylabel("price")
+#         plt.title("Degree {}\nMAPE = {:.3f}".format(degrees[i - 1], new_MAPE))
+#         plt.legend(loc="best")
+#
+#         # Recherche du degré aillant la meilleur peformance (ici MAPE), avec test de croissance et limite de croissance
+#         if (new_MAPE < MAPE and increasing_function(y_plot) and max(y_plot) < 13000):
+#             best_degree = degrees[i - 1]
+#             MAPE = new_MAPE
+#             best_prediction = y_plot[:, 0]
+#
+#     print("Meilleur degré " + str(best_degree))
+#
+#     # Affichage de la meilleure courbe de regression de l'arrondissement i
+#     xs = x_plot
+#     ys = arr.values[0][2] * np.ones(len(x_plot))
+#     zs = np.array(best_prediction)
+#     cx.plot(xs, ys, zs, label='Arrondissement ' + str(int(arr.values[0][2])))
+#
+# cx.set_ylabel('Arrondissement');
+# cx.set_xlabel('Surface');
+# cx.set_zlabel('Prix')
+# cx.view_init(10, -135)
+# cx.legend()
+# cx.set_title('Regression Polynomiale Spécifique')
 
 # %% Visualisation 3D
 
@@ -197,6 +197,7 @@ cx.set_title('Regression Polynomiale Spécifique')
 # ax.set_ylabel('Arrondissement'); ax.set_xlabel('Surface'); ax.set_zlabel('Prix')
 # ax.set_title('Représentation 3D du Dataset')
 # ax.view_init(10,0)
+# plt.show()
 
 # %% Projection 2D
 
