d7c0dca779d963567d16ca247c1b972b648ec81b
brianlz <brian.lozach@free.fr>
2018-10-16 17:50:51 +0200

    Few changes on Multilinear Regression to fit with the venv

--- DataScience/Titanic_Kaggle/Titanic_Prediction.py
+++ DataScience/Titanic_Kaggle/Titanic_Prediction.py
@@ -1,3 +1,43 @@
-import pandas as pd
+import importlib
+
+ pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+from sklearn.metrics import mean_squared_error
+
+# DataSets ---------------------------------------------
+train_raw = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/train.csv')
+Y_train = train_raw['Survived']
+X_train = train_raw.drop('Survived', 1)
+#X_train.drop('Cabin',1)
+
+Y_test = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/test.csv')
+
+
+
+# First Implementation Test : Regular Neural Net on Pclass ---------------------------------------------
+
+
+
+
+
+
+# Results ---------------------------------------------
+
+plt.plot(X_train['Age'], Y_train, 'bo', markersize=4)
+# plt.plot(X[:,1], regr.predict(X), color='r', linewidth=2)
+plt.title('Régression linéaire simple')
+plt.xlabel('Pclass')
+plt.ylabel('Survived')
+plt.show()
+
+
+
+
+
+
+
 
