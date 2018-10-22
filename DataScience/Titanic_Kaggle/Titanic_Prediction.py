d8ee87988348d6cebd06f0477fc17b9b2f2c0a97
brianlz <brian.lozach@free.fr>
2018-10-22 16:40:09 +0200

    Work on Titanic Datasets (Cleaning and Enrichment)

--- DataScience/Titanic_Kaggle/Titanic_Prediction.py
+++ DataScience/Titanic_Kaggle/Titanic_Prediction.py
@@ -1,39 +1,69 @@
-import importlib
+#LIBRAIRIES
 
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
-import sklearn
-from sklearn import model_selection
 
 # DATASETS ---------------------------------------------
-# Training Set
-train_raw = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/train.csv')
-Y_train = train_raw['Survived']
-X_train = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/training_set.csv')
-X_train = X_train.drop('Embarked_OLD', axis=1)
-X_train = X_train.drop('Sex_OLD', axis=1)
+
+X_train = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/train.csv')
+X_test = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/test.csv')
+
+data = [X_train, X_test]
+
+
+# DATA CLEANING ---------------------------------------------
+
+Y_train = X_train['Survived']
+
+X_train = X_train.drop('PassengerId',axis=1)
+X_train = X_train.drop('Ticket', axis=1)
+X_train = X_train.drop('Cabin',axis=1)
 X_train = X_train.drop('Survived', axis=1)
-# Testing Set
-X_test = pd.read_csv('/Users/brianlz/Documents/DataScience/Titanic_Kaggle/DataSets/testing_set.csv')
-X_test = X_test.drop('Embarked_OLD', axis=1)
-X_test = X_test.drop('Sex_OLD', axis=1)
+
+X_test = X_test.drop('PassengerId',axis=1)
+X_test = X_test.drop('Ticket', axis=1)
+X_test = X_test.drop('Cabin',axis=1)
 
 
-# FIRST IMPLEMENTATION : RANDOM FOREST  ---------------------------------------------
+for entry in data:
+    # entry['Embarked'] = entry['Embarked'].map({'S': 1, 'C': 2, 'Q': 3})
+    entry['Sex'] = entry['Sex'].map( {'male': 0, 'female': 1} ).astype(int)
 
-# X_train.drop('Cabin',1) ; X_train.drop('Name',1) ; X_train.drop('Ticket',1) ; X_train.drop('Age',1) ;
+# DATA ENRICHMENT ---------------------------------------------
 
-from sklearn.ensemble import RandomForestClassifier
+# Extract the Title of each passenger
 
-clf = RandomForestClassifier(n_estimators=1000)
-clf = clf.fit(X_train, Y_train)
+print(X_train['Sex'])
+print(X_train.shape)
 
-y_test = clf.predict(X_test)
-y_test = np.around(y_test.astype(int),0)
-np.savetxt("Results.csv", y_test, delimiter=",", fmt='%0.0f')
+
+
+
+
+
+
+# # FIRST IMPLEMENTATION : RANDOM FOREST  ---------------------------------------------
+#
+# # X_train.drop('Cabin',1) ; X_train.drop('Name',1) ; X_train.drop('Ticket',1) ; X_train.drop('Age',1) ;
+#
+# clf = RandomForestClassifier(n_estimators=50)
+# clf = clf.fit(X_train, Y_train)
+#
+# y_test = clf.predict(X_test)
+# y_test = np.around(y_test.astype(int),0)
+# np.savetxt("Results.csv", y_test, delimiter=",", fmt='%0.0f')
+#
+# # FIRST IMPLEMENTATION : SUPPORT VECTOR MACHINE  ---------------------------------------------
+#
+# svc = SVC()
+# svc.fit(X_train, Y_train)
+# Y_pred = svc.predict(X_test)
+# Y_pred = np.around(y_test.astype(int),0)
+# np.savetxt("ResultsSVC.csv", y_test, delimiter=",", fmt='%0.0f')
 
 
 # Results ---------------------------------------------
